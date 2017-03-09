bbe897640b78cbd1edc584b4c57bea923414eb2d
Amit <pl-amit@users.noreply.github.com>
2017-03-09 16:43:03 +0530

    Add files via upload

---
+++ samples.go
@@ -0,0 +1,61 @@
+package main
+
+var samples = `
+{
+    "event": {
+		"assetID": "1234",
+		"status": "In-Transit",
+		"role": "Fleet Operator",
+		"lastowner": "J&J",
+		"ownername": "DHL",
+		"ownerid": "3",
+		"overallstatus": "Good",
+		"latitude": "123.456",
+		"longitude": "123.456",
+		"luminosity": "28",
+		"humidity": "39",
+		"vibration": "1.4",
+		"pressure": "20",
+		"temperature": "-4.0",
+		"time": "12:34:56:78",
+		"timestamp": "1234567890",
+		"orderid": "1234",
+		"container": "SC20515",
+		"orderdate": "02/02/2017",
+		"content": "Dummy Dummy",
+		"health": "99.8",
+		"customername": "Hospital",
+		"destination": "Mumbai",
+		"country": "India"
+    },
+    "initEvent": {
+        "nickname": "SIMPLE",
+        "version": "The ID of a managed asset. The resource focal point for a smart contract."
+    },
+    "state": {
+		"assetID": "1234",
+		"status": "In-Transit",
+		"role": "Fleet Operator",
+		"lastowner": "J&J",
+		"ownername": "DHL",
+		"ownerid": "3",
+		"overallstatus": "Good",
+		"latitude": "123.456",
+		"longitude": "123.456",
+		"luminosity": "28",
+		"humidity": "39",
+		"vibration": "1.4",
+		"pressure": "20",
+		"temperature": "-4.0",
+		"time": "12:34:56:78",
+		"timestamp": "1234567890",
+		"orderid": "1234",
+		"container": "SC20515",
+		"orderdate": "02/02/2017",
+		"content": "Dummy Dummy",
+		"health": "99.8",
+		"customername": "Hospital",
+		"destination": "Mumbai",
+		"country": "India"
+    }
+}`
